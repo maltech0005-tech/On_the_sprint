2def323a9433680cc6eebba7a893366e4bd01ca1
anotida <maltech0005@gmail.com>
2025-12-16 18:00:46 +0200

    updated character animation in motion and fixed wakatime error
    now character can now punch using the 'p' key kick using the 'k' key and all current motion animation s are working on point even extended the road size then fixed wakatime error by replacing the config file by the one from BudzioT's repo

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,13 +1,47 @@
 extends CharacterBody2D
 
-
-const SPEED = 300.0
-const JUMP_VELOCITY = -450
+const SPEED = 150.0
+const JUMP_VELOCITY = -500
 
 @onready var animatedsprite = $AnimatedSprite2D
+@onready var jump_collision = $jump_CollisionShape2D
+@onready var kick_punch_collision = $CollisionPolygon2D
+@onready var idle_collision = $idle_CollisionShape2D
 
-# player movement animation
 
+
+# collision shape deactivation
+func deactivate_collision():
+	jump_collision.disabled = true
+	kick_punch_collision.disabled = true
+	idle_collision.disabled = true
+# player movement animation and collision shape activation
+
+
+func animated_movement():
+	
+	if not is_on_floor() and velocity.y > 0:
+		if velocity.y < 0:
+			animatedsprite.play("jump_up")
+		elif velocity.y > 0:
+			animatedsprite.play("jump_down")
+		deactivate_collision()
+		jump_collision.disabled = false
+	if is_on_floor():
+		if not Input.is_anything_pressed():
+			animatedsprite.play("idle")
+		deactivate_collision()
+		idle_collision.disabled = false
+	if Input.is_action_pressed("punch"):
+		animatedsprite.play("punch")
+		deactivate_collision()
+		kick_punch_collision.disabled = false
+		
+	if Input.is_action_pressed("kick"):
+		animatedsprite.play("kick")
+		deactivate_collision()
+		kick_punch_collision.disabled = false
+		
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
@@ -29,7 +63,9 @@ func _physics_process(delta: float) -> void:
 		
 	if direction:
 		velocity.x = direction * SPEED
+		animatedsprite.play("run")
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
+	animated_movement()
 	move_and_slide()
