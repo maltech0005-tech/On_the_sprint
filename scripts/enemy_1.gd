b3cca96f0a8c236df392b790cb629da8a560a194
anotida <maltech0005@gmail.com>
2025-12-20 17:07:58 +0200

    added get player direction logic

--- scripts/enemy_1.gd
+++ scripts/enemy_1.gd
@@ -7,24 +7,42 @@ extends CharacterBody2D
 @onready var sprite = $AnimatedSprite2D2
 @onready var bullet_sprite = $bullet/bullet_sprite
 @onready var receive_damage = $receive_damage
-@onready var player = $player
 
+var player = null
 var health = 20
-var direction = Vector2()
 var projectile_speed = 120
 var is_in_range = false
 var attack_interval = 30
 
+func _ready():
+	var players = get_tree().get_nodes_in_group("player")
+	if players.size()>0:
+		player = players[0]
+
 func _physics_process(delta: float) -> void:
-	if is_in_range:
-		bullet.global_position.x += projectile_speed*delta*-1
-		sprite.flip_h=true
-		bullet_sprite.flip_h=true
+	if player:
+		var player_position = player.global_position
+		var direction = (player.global_position-global_position).normalized
+		bullet_sprite.play("idle")
+		if player_position.x < global_position.x:
+			sprite.flip_h=true
+			bullet_sprite.flip_h=true
+			bullet.global_position.x += projectile_speed*-1*delta
+		
+		if player_position.x > global_position.x:
+			sprite.flip_h=false
+			bullet_sprite.flip_h=false
+			bullet.global_position.x += projectile_speed*delta
+			
+		if is_in_range:
+			bullet_sprite.play("")
+			
+			
+	
 		
 func _on_bullet_body_entered(body: Node2D) -> void:
 	if body.is_in_group("player"):
 		timer.start(attack_interval)
-		bullet_sprite.play("explode")
 		body.take_damage(10)
 		bullet.global_position=global_position
 		
@@ -44,7 +62,7 @@ func _on_receive_damage_body_entered(body: Node2D) -> void:
 		health -= 1
 		if health >= 0:
 			queue_free()
-	
+
 func _on_timer_timeout() -> void:
 	if is_in_range:
 		timer.start(attack_interval)
